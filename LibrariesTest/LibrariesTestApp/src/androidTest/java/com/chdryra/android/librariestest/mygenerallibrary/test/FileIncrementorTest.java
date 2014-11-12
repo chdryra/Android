a41df3640d590f3d675086c8b72aabf07acfd05c
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2014-11-12 19:40:25 +0000

    FileIncrementor test

--- LibrariesTest/LibrariesTestApp/src/androidTest/java/com/chdryra/android/librariestest/mygenerallibrary/test/FileIncrementorTest.java
+++ LibrariesTest/LibrariesTestApp/src/androidTest/java/com/chdryra/android/librariestest/mygenerallibrary/test/FileIncrementorTest.java
@@ -8,7 +8,6 @@
 
 package com.chdryra.android.librariestest.mygenerallibrary.test;
 
-import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.MediumTest;
 
@@ -30,7 +29,7 @@ public class FileIncrementorTest extends ActivityInstrumentationTestCase2<Testin
     private static final String EXT       = "txt";
     private static final int    NUM_FILES = 5;
 
-    private Activity mActivity;
+    private File mExternalDir;
 
     public FileIncrementorTest() {
         super(TestingActivity.class);
@@ -39,51 +38,121 @@ public class FileIncrementorTest extends ActivityInstrumentationTestCase2<Testin
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-
-        mActivity = getActivity();
+        mExternalDir = getInstrumentation().getTargetContext().getDir(EXTDIR, 0);
     }
 
     @Override
     protected void tearDown() throws Exception {
+        deleteRecursive(mExternalDir);
+        assertFalse(mExternalDir.exists());
         super.tearDown();
-        deleteRecursive(getExternalDirectory());
     }
 
     @MediumTest
-    public void testFileCreation() {
+    public void testCreateFile() {
         File file = getFile();
-
         assertFalse(file.exists());
 
-        try {
-            getFileIncrementor().createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        File created = createFile(getFileIncrementor());
+        assertNotNull(created);
+        assertFalse(created.isDirectory());
         assertTrue(file.exists());
+        assertEquals(file, created);
     }
 
     @MediumTest
-    public void testMultipleFileCreationAndRemoval() {
+    public void testCreateFileMultiple() {
+        File dir = getDir();
+        assertNull(dir.list());
+        FileIncrementor incrementor = getFileIncrementor();
+        for (int i = 0; i < NUM_FILES; ++i) {
+            assertEquals(getFile(i), createFile(incrementor));
+            assertEquals(i + 1, dir.list().length);
+        }
+    }
+
+    @MediumTest
+    public void testLastFile() {
+        File file = getFile();
+        assertFalse(file.exists());
+
+        //Create first file
+        FileIncrementor incrementor = getFileIncrementor();
+        File created = createFile(incrementor);
+        assertNotNull(created);
+        assertFalse(created.isDirectory());
+        assertTrue(file.exists());
+        assertEquals(file, created);
+
+        //Create second file
+        File file1 = getFile(1);
+        assertFalse(file1.exists());
+        File created1 = createFile(incrementor);
+        assertNotNull(created1);
+        assertFalse(created1.isDirectory());
+        assertTrue(file1.exists());
+        assertEquals(file1, created1);
+
+        //Delete 2nd file
+        incrementor.deleteLastFile();
+        assertFalse(file1.exists());
+        assertEquals(file1, created1);
+
+        //Ensure 1st file still around
+        assertFalse(created.isDirectory());
+        assertTrue(file.exists());
+        assertEquals(file, created);
+
+        //Create second file again
+        File created1_2 = createFile(incrementor);
+        assertNotNull(created1_2);
+        assertFalse(created1_2.isDirectory());
+        assertTrue(file1.exists());
+        assertEquals(file1, created1_2);
+    }
+
+    @MediumTest
+    public void testDeleteCreatedFiles() {
         File dir = getDir();
         assertNull(dir.list());
 
         FileIncrementor incrementor = getFileIncrementor();
-        for (int i = 0; i < NUM_FILES; ++i) {
-            try {
-                incrementor.createNewFile();
-                assertEquals(i + 1, dir.list().length);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        createAndTestMultipleFiles(incrementor);
 
         assertEquals(NUM_FILES, dir.list().length);
-
-        incrementor.clearDirectory();
-
+        incrementor.deleteCreatedFiles();
         assertEquals(0, dir.list().length);
+    }
+
+    @MediumTest
+    public void testDeleteCreatedDirectory() {
+        File dir = getDir();
+        assertFalse(dir.exists());
+
+        FileIncrementor incrementor = getFileIncrementor();
+        createAndTestMultipleFiles(incrementor);
+
+
+        assertTrue(dir.exists());
+        incrementor.deleteCreatedDirectory();
+        assertFalse(dir.exists());
+    }
+
+    private File createFile(FileIncrementor incrementor) {
+        File file = null;
+        try {
+            file = incrementor.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return file;
+    }
+
+    private void createAndTestMultipleFiles(FileIncrementor incrementor) {
+        for (int i = 0; i < NUM_FILES; ++i) {
+            assertEquals(getFile(i), createFile(incrementor));
+        }
     }
 
     private void deleteRecursive(File fileOrDirectory) {
@@ -96,19 +165,20 @@ public class FileIncrementorTest extends ActivityInstrumentationTestCase2<Testin
         fileOrDirectory.delete();
     }
 
-    private File getExternalDirectory() {
-        return getInstrumentation().getTargetContext().getDir(EXTDIR, 0);
-    }
-
     private File getDir() {
-        return new File(getExternalDirectory(), DIR);
+        return new File(mExternalDir, DIR);
     }
 
     private File getFile() {
         return new File(getDir(), FILE + "." + EXT);
     }
 
+    private File getFile(int suffix) {
+        if (suffix == 0) return getFile();
+        return new File(getDir(), FILE + "_" + String.valueOf(suffix) + "." + EXT);
+    }
+
     private FileIncrementor getFileIncrementor() {
-        return new FileIncrementor(getExternalDirectory().getPath(), DIR, FILE, EXT);
+        return new FileIncrementor(mExternalDir, DIR, FILE, EXT);
     }
 }
