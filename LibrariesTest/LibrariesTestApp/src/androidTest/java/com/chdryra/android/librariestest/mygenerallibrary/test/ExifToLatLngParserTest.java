529d3876eb004c7885a2e685d9b9737746a90493
Rizwan Choudrey <rizwan.choudrey@gmail.com>
2014-11-14 18:11:51 +0000

    Changed ExifToLatLngConverter to ExifToLatLngParser due to AS bug of not being able to add files to git if originally say no when prompted. Split out from ImageHelper. ImageHelper now only deals with images given a filepath (via static methods) - no more responsibility for creating/deleting files.
     Review has ImageChooser that deals with finding all Intents that can respond to getting an image, and deals with creatin, saving and deleting image files if Intents cancelled or return null.

--- LibrariesTest/LibrariesTestApp/src/androidTest/java/com/chdryra/android/librariestest/mygenerallibrary/test/ExifToLatLngParserTest.java
+++ LibrariesTest/LibrariesTestApp/src/androidTest/java/com/chdryra/android/librariestest/mygenerallibrary/test/ExifToLatLngParserTest.java
@@ -12,7 +12,7 @@ import android.media.ExifInterface;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.util.Log;
 
-import com.chdryra.android.mygenerallibrary.ExifToLatLngConverter;
+import com.chdryra.android.mygenerallibrary.ExifToLatLngParser;
 import com.google.android.gms.maps.model.LatLng;
 
 import junit.framework.TestCase;
@@ -26,8 +26,8 @@ import java.util.Random;
  * On: 13/11/2014
  * Email: rizwan.choudrey@gmail.com
  */
-public class ExifToLatLngConverterTest extends TestCase {
-    private static final String TAG = "ExifToLatLngConverterTest";
+public class ExifToLatLngParserTest extends TestCase {
+    private static final String TAG = "ExifToLatLngParserTest";
 
     private static final String DRIVE = "C:";
     private static final String FILE  = "dummy.jpg";
@@ -35,7 +35,7 @@ public class ExifToLatLngConverterTest extends TestCase {
 
     @Override
     protected void setUp() throws Exception {
-        testDec2DMS();
+        dec2DMSTest();
     }
 
     @SmallTest
@@ -57,33 +57,33 @@ public class ExifToLatLngConverterTest extends TestCase {
 
         ExifInterface exif = getExif(0, 0);
         exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE, null);
-        ExifToLatLngConverter converter = new ExifToLatLngConverter(exif);
-        assertFalse(converter.isValid());
+        ExifToLatLngParser Parser = new ExifToLatLngParser(exif);
+        assertFalse(Parser.isValid());
 
         exif = getExif(0, 0);
         exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE, null);
-        converter = new ExifToLatLngConverter(exif);
-        assertFalse(converter.isValid());
+        Parser = new ExifToLatLngParser(exif);
+        assertFalse(Parser.isValid());
 
         exif = getExif(0, 0);
         exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE_REF, null);
-        converter = new ExifToLatLngConverter(exif);
-        assertFalse(converter.isValid());
+        Parser = new ExifToLatLngParser(exif);
+        assertFalse(Parser.isValid());
 
         exif = getExif(0, 0);
         exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF, null);
-        converter = new ExifToLatLngConverter(exif);
-        assertFalse(converter.isValid());
+        Parser = new ExifToLatLngParser(exif);
+        assertFalse(Parser.isValid());
 
         exif = getExif(0, 0);
         exif.setAttribute(ExifInterface.TAG_GPS_LATITUDE_REF, "X");
-        converter = new ExifToLatLngConverter(exif);
-        assertFalse(converter.isValid());
+        Parser = new ExifToLatLngParser(exif);
+        assertFalse(Parser.isValid());
 
         exif = getExif(0, 0);
         exif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF, "X");
-        converter = new ExifToLatLngConverter(exif);
-        assertFalse(converter.isValid());
+        Parser = new ExifToLatLngParser(exif);
+        assertFalse(Parser.isValid());
     }
 
     @SmallTest
@@ -97,16 +97,16 @@ public class ExifToLatLngConverterTest extends TestCase {
     private boolean getIsValid(double lat, double lng) {
         ExifInterface exif = getExif(lat, lng);
 
-        ExifToLatLngConverter converter = new ExifToLatLngConverter(exif);
-        return converter.isValid();
+        ExifToLatLngParser Parser = new ExifToLatLngParser(exif);
+        return Parser.isValid();
     }
 
     private void testGetLatLng(double lat, double lng) {
         ExifInterface exif = getExif(lat, lng);
 
-        ExifToLatLngConverter converter = new ExifToLatLngConverter(exif);
-        LatLng latLng = converter.getLatLng();
-        assertTrue(converter.isValid());
+        ExifToLatLngParser parser = new ExifToLatLngParser(exif);
+        LatLng latLng = parser.getLatLng();
+        assertTrue(parser.isValid());
         assertEquals(lat, latLng.latitude, EPS);
         assertEquals(lng, latLng.longitude, EPS);
     }
@@ -148,7 +148,7 @@ public class ExifToLatLngConverterTest extends TestCase {
     }
 
 
-    private void testDec2DMS() {
+    private void dec2DMSTest() {
         String zeroDMS = getDMSFromDecimalCoordinate(0);
 
         String[] sDMS = zeroDMS.split(",", 3);
@@ -170,7 +170,7 @@ public class ExifToLatLngConverterTest extends TestCase {
         sM = sDMS[1].split("/", 2);
         sS = sDMS[2].split("/", 2);
 
-        //Using online converter
+        //Using online Parser
         assertEquals(Double.valueOf(sD[0]), 3, EPS);
         assertEquals(Double.valueOf(sD[1]), 1, EPS);
         assertEquals(Double.valueOf(sM[0]), 8, EPS);
